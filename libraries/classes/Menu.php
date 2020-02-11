d59d76b64ff605f7d03fbe6836051f4bd67fb8bd
Stefan Geith <development@geithware.de>
2020-02-11 11:08:11 +0100

    Fix lost username and host in title

--- libraries/classes/Menu.php
+++ libraries/classes/Menu.php
@@ -202,7 +202,14 @@ class Menu
         }
         $item .= '%3$s</a>';
         $retval .= "<div id='floating_menubar'></div>";
-        $retval .= "<div id='serverinfo'>";
+		$mysql_cur_user_and_host = $GLOBALS['dbi']->fetchValue('SELECT USER();');
+		if (strncmp($mysql_cur_user_and_host,'cpl_ro',4)===0 || strncmp($mysql_cur_user_and_host,'readonly',4)===0) {
+		    $retval .= "<div id='serverinfo' style='background-color:#00b000;'>";
+		} elseif (strncmp($mysql_cur_user_and_host,'root',4)===0) {
+		    $retval .= "<div id='serverinfo' style='background-color:#b00000;'>";
+		} else {
+	        $retval .= "<div id='serverinfo'>";
+		}
         if (Util::showIcons('TabsMode')) {
             $retval .= Util::getImage(
                 's_host',
@@ -320,7 +327,8 @@ class Menu
                 }
             }
         }
-        $retval .= '<div class="clearfloat"></div>';
+		$retval .= ' &nbsp; &nbsp; || &nbsp; <span style="color:#ffff80;">USER='.$mysql_cur_user_and_host.'</span>';
+		$retval .= '<div class="clearfloat"></div>';
         $retval .= '</div>';
         return $retval;
     }
